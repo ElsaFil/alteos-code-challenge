0129f1b8a3ba0adfe0e2d56c2ecc161d7e7673e4
Elsa Filippidou <elsa.filippidou@gmail.com>
2020-03-19 20:26:54 +0100

    simple styling

--- client/src/components/ContactCard.js
+++ client/src/components/ContactCard.js
@@ -6,7 +6,40 @@ class ContactCard extends Component {
 
     return (
       <div className="contact-card">
-        <h4>{contact.name}</h4>
+        <h3>{contact.name}</h3>
+        <section className="general-info">
+          <div>
+            <span className="title">Age: </span>
+            <span className="value">{contact.age}</span>
+          </div>
+          <div>
+            <span className="title">Gender: </span>
+            <span className="value">{contact.gender}</span>
+          </div>
+          <div>
+            <span className="title">Eye color: </span>
+            <span className="value">{contact.eyeColor}</span>
+          </div>
+        </section>
+        <hr className="separator" />
+        <section className="contact-info">
+          <div>
+            <span className="title">Company: </span>
+            <span className="value">{contact.company}</span>
+          </div>
+          <div>
+            <span className="title">Email: </span>
+            <span className="value">{contact.email}</span>
+          </div>
+          <div>
+            <span className="title">Phone: </span>
+            <span className="value">{contact.phone}</span>
+          </div>
+          <div>
+            <span className="title">Address: </span>
+            <span className="value">{contact.address}</span>
+          </div>
+        </section>
       </div>
     );
   }
